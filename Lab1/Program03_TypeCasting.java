76b4a51f947abdab0057df3ac32949747310db38
saiprasaad <saiprasaad@gmail.com>
2021-02-19 23:22:37 +0530

    Create Program03_TypeCasting.java

---
+++ Lab1/Program03_TypeCasting.java
@@ -0,0 +1,27 @@
+package lab1;
+
+public class Program03_TypeCasting {
+	public static void main(String[] args) {
+		int intvar=1;
+		long longvar=2;
+		float floatvar=1.1f;
+		double doublevar=2.2;
+		char charvar=' ';
+		System.out.println("Integer to long "+(long)intvar);
+		System.out.println("Integer to float "+(float)intvar);
+		System.out.println("Integer to double "+(double)intvar);
+		System.out.println("Integer to Character "+(char)intvar);
+		System.out.println("Long to int "+(int)longvar);
+		System.out.println("Long to float "+(float)longvar);
+		System.out.println("Long to double "+(double)longvar);
+		System.out.println("Long to Character "+(char)longvar);
+		System.out.println("Float to int "+(int)floatvar);
+		System.out.println("Float to long "+(long)floatvar);
+		System.out.println("Float to double "+(double)floatvar);
+		System.out.println("Float to Character "+(char)floatvar);
+		System.out.println("Character to int "+(int)charvar);
+		System.out.println("Character to long "+(long)charvar);
+		System.out.println("Character to float "+(float)charvar);
+		System.out.println("Character to double "+(double)charvar);
+	}
+}
