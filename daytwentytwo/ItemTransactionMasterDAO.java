8ee4210331f292456bc5f8dd7f28675c5b887299
saiprasaad <saiprasaad@gmail.com>
2021-03-16 16:46:37 +0530

    update

---
+++ daytwentytwo/ItemTransactionMasterDAO.java
@@ -0,0 +1,11 @@
+package daytwentytwo;
+
+import java.util.Set;
+
+public interface ItemTransactionMasterDAO {
+	public int insertItem(ItemTransactionMasterDTO itemTransactionMasterDTO);
+	public int deleteItem(int invno,int itemno);
+	public int updateItem(ItemTransactionMasterDTO itemTransactionMasterDTO);
+	public ItemTransactionMasterDTO getItemTransactionMaster(int invno,int itemno);
+	public Set<ItemTransactionMasterDTO> getItemTransactionMasterAll();
+}
